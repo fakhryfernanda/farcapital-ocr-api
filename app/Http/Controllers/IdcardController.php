84cc838a72827b51561f20873e521d873a06134c
khoiruddoa <khoiruddoa1995@gmail.com>
2023-01-05 16:36:23 +0700

    solved read id card algoritm

--- app/Http/Controllers/IdcardController.php
+++ app/Http/Controllers/IdcardController.php
@@ -22,211 +22,290 @@ class IdcardController extends Controller
 
     public function readImage(Request $request)
     {
-
-
         $image = $request->image;
         if (isset($image) && $image->getPathName()) {
             $ocr = app()->make(OcrAbstract::class);
             $parsedText = $ocr->scan($image->getPathName());
 
 
-            $newParsedText = preg_split('/\n/', $parsedText);
-            $newParsedText = array_values(array_filter($newParsedText));
-            $provinsi = $newParsedText[0];
-
-            $cek_provinsi = explode(" ", $provinsi);
 
 
-            if ($cek_provinsi[0] == 'PROVINSI') {
+            $pattern = '/provins/i';
+            $checkProvinsi = preg_match($pattern, $parsedText, $matches);
 
 
-                $pattern = "/(?<=provinsi ).*/i";
-                preg_match($pattern, $provinsi, $matches);
-                $provinsi = $matches[0];
-
-                // -----batas suci-------
-                $kota = $newParsedText[1];
-
-                // -----batas suci-------
-                $pattern = "/(?<=nik ).*/i";
-                $nik = $newParsedText[2];
-                preg_match($pattern, $nik, $matches);
-                $nik = $matches[0];
-                $pattern = "/[0-9]+/i";
-                preg_match($pattern, $nik, $matches);
-                $nik = $matches[0];
-
-                // -----batas suci-------
-                $pattern = "/(?<=nama ).*/i";
-                $nama = $newParsedText[3];
-                preg_match($pattern, $nama, $matches);
-                $nama = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match_all($pattern, $nama, $anjay);
-
-                $nama = implode(" ", $anjay[0]);
-
-                // -----batas suci-------
-                $pattern = "/\d{2}-\d{2}-\d{4}/i";
-                $tanggal_lahir = $newParsedText[4];
-                preg_match($pattern, $tanggal_lahir, $matches);
-                $tanggal_lahir = $matches[0];
-
-                // -----batas suci-------
-                $pattern = "/(?<=Lahir).*(?=" . $tanggal_lahir . ")/i";
-                $tempat_lahir = $newParsedText[4];
-                preg_match($pattern, $tempat_lahir, $matches);
-                $tempat_lahir = $matches[0];
-                $pattern = "/\w+/i";
-                preg_match_all($pattern, $tempat_lahir, $hilih);
-                $tempat_lahir = implode(" ", $hilih[0]);
-
-                // -----batas suci-------
-                $pattern = "/(?<=Darah).*/i";
-                $goldar = $newParsedText[5];
-                preg_match($pattern, $goldar, $matches);
-
-                $pattern = "/[ABO-]+/i";
-                preg_match($pattern, $matches[0], $matches);
-                $golongan_darah = $matches[0];
-
-                // -----batas suci-------
-                $pattern = "/(?<=kelamin).*/i";
-                $goldar = $newParsedText[5];
-                preg_match($pattern, $goldar, $matches);
-                // dd($matches);
-                $pattern = "/[LP]+/";
-                preg_match($pattern, $matches[0], $matches);
-                $gender = $matches[0];
-                // dd($gender);
-
-                if ($gender == "p" || $gender == "P") {
-                    $gender = 'PEREMPUAN';
-                } else {
-                    $gender = "LAKI-LAKI";
-                }
-
-                // -----batas suci-------
-                $pattern = "/(?<=alamat).*/i";
-                $alamat = $newParsedText[6];
-                preg_match($pattern, $alamat, $matches);
-                $alamat = $matches[0];
-                $pattern = "/(?=[a-z]).*/i";
-                preg_match_all($pattern, $alamat, $anjay);
-
-                $alamat = implode(" ", $anjay[0]);
-
-                // -----batas suci-------
-                $pattern = "/(?=[0-9]).*/i";
-                preg_match_all($pattern, $newParsedText[7], $hilih);
-                // $rtw = explode(":", $newParsedText[7]);
-                $rtw = explode("/", $hilih[0][0]);
-                $rt = trim($rtw[0], " ");
-                $rw = trim($rtw[1], " ");
-
-                // -----batas suci-------
-                $pattern = "/(?<=Desa ).*/i";
-                $kelurahan = $newParsedText[8];
-                preg_match($pattern, $kelurahan, $matches);
-                $kelurahan = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match_all($pattern, $kelurahan, $anjay);
-
-                $kelurahan = implode(" ", $anjay[0]);
-
-                // -----batas suci-------
-                $pattern = "/(?<=camatan ).*/i";
-                $kecamatan = $newParsedText[9];
-                preg_match($pattern, $kecamatan, $matches);
-                $kecamatan = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match_all($pattern, $kecamatan, $anjay);
-
-                $kecamatan = implode(" ", $anjay[0]);
-                // $kec = explode(":", $newParsedText[9]);
-                // $kec = trim($kec[1], " "); //RIP
-                // $newParsedText[9] = $kec;
-
-                // -----batas suci-------
-                $pattern = "/(?<=agama ).*/i";
-                $agama = $newParsedText[10];
-                // dd($agama);
-                preg_match($pattern, $agama, $matches);
-                $agama = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match($pattern, $agama, $matches);
-                $agama = $matches[0];
-                $newParsedText[10] = $agama;
-
-                // -----batas suci-------
-                $pattern = "/(?<=kawinan ).*/i";
-                $perkawinan = $newParsedText[11];
-                preg_match($pattern, $perkawinan, $matches);
-                $perkawinan = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match_all($pattern, $perkawinan, $anjay);
-
-                $perkawinan = implode(" ", $anjay[0]);
-
-                // -----batas suci-------
-                $pattern = "/(?<=kerjaan ).*/i";
-                $pekerjaan = $newParsedText[12];
-                preg_match($pattern, $pekerjaan, $matches);
-                $pekerjaan = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match_all($pattern, $pekerjaan, $anjay);
-
-                $pekerjaan = implode(" ", $anjay[0]);
-
-                // -----batas suci-------
-                $pattern = "/(?<=negaraan ).*/i";
-                $kewarganegaraan = $newParsedText[13];
-                preg_match($pattern, $kewarganegaraan, $matches);
-                $kewarganegaraan = $matches[0];
-                $pattern = "/[a-z]+/i";
-                preg_match($pattern, $kewarganegaraan, $anjay);
-
-                $kewarganegaraan = $anjay[0];
-                // dd($newParsedText);
-                $ktp = [
-                    "provinsi" => $provinsi,
-                    "kota" => $kota,
-                    "nik" => $nik,
-                    "nama" => $nama,
-                    "tempat_lahir" => $tempat_lahir,
-                    "tanggal_lahir" => $tanggal_lahir,
-                    "kelamin" => $gender,
-                    "golongan_darah" => $golongan_darah,
-                    "alamat" => $alamat,
-                    "rt" => $rt,
-                    "rw" => $rw,
-                    "kelurahan" => $kelurahan,
-                    "kecamatan" => $kecamatan,
-                    "agama" => $agama,
-                    "perkawinan" => $perkawinan,
-                    "pekerjaan" => $pekerjaan,
-                    "kewarganegaraan" => $kewarganegaraan
-                ];
+            $new_pattern = preg_split('/\n/', $parsedText);
+            $new_pattern = array_values(array_filter($new_pattern));
 
 
-                return response()->json([
-                    'status' => true,
-                    'message' => 'Upload file berhasil',
-                    'data' => $ktp
-                ]);
+            if ($new_pattern == null) {
+                dd('mohon upload ulang ktp');
             } else {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Mohon Upload Ulang KTP anda',
-                    'data' => null
-                ]);
+                $provinsi = $new_pattern[0];
+                if ($checkProvinsi) {
+
+
+
+                    $pattern = "/(?<=provinsi ).*/i";
+                    $isExisted = preg_match($pattern, $provinsi, $matches);
+                    if ($isExisted == 1) {
+                        $provinsi = $matches[0];
+                    } else {
+                        $provinsi = '-';
+                    }
+
+                    // -----batas suci-------
+                    $kota = $new_pattern[1];
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=nik ).*/i";
+                    $nik = $new_pattern[2];
+                    $isExisted = preg_match($pattern, $nik, $matches);
+                    if ($isExisted === 1) {
+                        $nik = $matches[0];
+                        $pattern = "/[0-9]+/i";
+                        $isExisted = preg_match($pattern, $nik, $matches);
+                        if ($isExisted == 1) {
+                            $nik = $matches[0];
+                        } else {
+                            $nik = '-';
+                        }
+                    } else {
+                        $nik = '-';
+                    }
+
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=nama ).*/i";
+                    $nama = $new_pattern[3];
+                    $isExisted = preg_match($pattern, $nama, $matches);
+                    if ($isExisted == 1) {
+                        $nama = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match_all($pattern, $nama, $anjay);
+
+                        $nama = implode(" ", $anjay[0]);
+                    } else {
+                        $nama = '-';
+                    }
+
+
+
+                    // -----batas suci-------
+                    $pattern = "/\d{2}-\d{2}-\d{4}/i";
+                    $tanggal_lahir = $new_pattern[4];
+                    $isExisted = preg_match($pattern, $tanggal_lahir, $matches);
+                    if ($isExisted == 1) {
+                        $tanggal_lahir = $matches[0];
+
+                        if ($tanggal_lahir !== null) {
+                            $tanggal_lahir = $matches[0];
+                        } else {
+                            $tanggal_lahir = '-';
+                        }
+
+                        // -----batas suci-------
+                        $pattern = "/(?<=Lahir).*(?=" . $tanggal_lahir . ")/i";
+                        $tempat_lahir = $new_pattern[4];
+                        $isExisted =  preg_match($pattern, $tempat_lahir, $matches);
+                        if ($isExisted == 1) {
+                            $tempat_lahir = $matches[0];
+                            $pattern = "/\w+/i";
+                            preg_match_all($pattern, $tempat_lahir, $hilih);
+                            $tempat_lahir = implode(" ", $hilih[0]);
+                        } else {
+                            $tempat_lahir = '-';
+                        }
+                    } else {
+                        $tanggal_lahir = "-";
+                        $tempat_lahir = "-";
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=Darah).*/i";
+                    $goldar = $new_pattern[5];
+                    $isExisted = preg_match($pattern, $goldar, $matches);
+                    if ($isExisted == 1) {
+                        // dd($matches);
+                        $pattern = "/[ABO-]+/i";
+                        $isExisted = preg_match($pattern, $matches[0], $matches);
+                        if ($isExisted == 1) {
+                            $golongan_darah = $matches[0];
+                        } else {
+                            $golongan_darah = '-';
+                        }
+                    } else {
+                        $golongan_darah = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=kelamin).*/i";
+                    $goldar = $new_pattern[5];
+                    $isExisted = preg_match($pattern, $goldar, $matches);
+                    if ($isExisted == 1) {
+                        // dd($matches);
+                        $pattern = "/[LP]+/";
+                        $isExisted = preg_match($pattern, $matches[0], $matches);
+                        if ($isExisted == 1) {
+                            $gender = $matches[0];
+
+                            // dd($gender);
+
+                            if ($gender == "p" || $gender == "P") {
+                                $gender = 'PEREMPUAN';
+                            } else {
+                                $gender = "LAKI-LAKI";
+                            }
+                        } else {
+                            $gender = '-';
+                        }
+                    } else {
+                        $gender = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=alamat).*/i";
+                    $alamat = $new_pattern[6];
+                    $isExisted = preg_match($pattern, $alamat, $matches);
+                    if ($isExisted == 1) {
+                        $alamat = $matches[0];
+                        $pattern = "/(?=[a-z]).*/i";
+                        preg_match_all($pattern, $alamat, $anjay);
+
+
+                        $alamat = implode(" ", $anjay[0]);
+                    } else {
+                        $alamat = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?=[0-9]).*/i";
+                    $isExisted = preg_match_all($pattern, $new_pattern[7], $hilih);
+                    // $rtw = explode(":", $new_pattern[7]);
+                    if ($isExisted == 1) {
+                        $rtw = explode("/", $hilih[0][0]);
+                        $rt = trim($rtw[0], " ");
+                        $rw = trim($rtw[1], " ");
+                    } else {
+                        $rt = '-';
+                        $rw = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=Desa ).*/i";
+                    $kelurahan = $new_pattern[8];
+                    $isExisted = preg_match($pattern, $kelurahan, $matches);
+                    if ($isExisted == 1) {
+                        $kelurahan = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match_all($pattern, $kelurahan, $anjay);
+
+
+                        $kelurahan = implode(" ", $anjay[0]);
+                    } else {
+                        $kelurahan = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=camatan ).*/i";
+                    $kecamatan = $new_pattern[9];
+                    $isExisted = preg_match($pattern, $kecamatan, $matches);
+                    if ($isExisted == 1) {
+                        $kecamatan = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match_all($pattern, $kecamatan, $anjay);
+
+                        $kecamatan = implode(" ", $anjay[0]);
+                    } else {
+                        $kecamatan = '-';
+                    }
+                    // $kec = explode(":", $new_pattern[9]);
+                    // $kec = trim($kec[1], " "); //RIP
+                    // $new_pattern[9] = $kec;
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=agama ).*/i";
+                    $agama = $new_pattern[10];
+                    // dd($agama);
+                    $isExisted = preg_match($pattern, $agama, $matches);
+                    if ($isExisted == 1) {
+                        $agama = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match($pattern, $agama, $matches);
+                        $agama = $matches[0];
+                    } else {
+                        $agama = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=kawinan ).*/i";
+                    $perkawinan = $new_pattern[11];
+                    $isExisted = preg_match($pattern, $perkawinan, $matches);
+                    if ($isExisted == 1) {
+                        $perkawinan = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match_all($pattern, $perkawinan, $anjay);
+
+                        $perkawinan = implode(" ", $anjay[0]);
+                    } else {
+                        $perkawinan = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=kerjaan ).*/i";
+                    $pekerjaan = $new_pattern[12];
+                    $isExisted = preg_match($pattern, $pekerjaan, $matches);
+                    if ($isExisted == 1) {
+                        $pekerjaan = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match_all($pattern, $pekerjaan, $anjay);
+
+                        $pekerjaan = implode(" ", $anjay[0]);
+                    } else {
+                        $pekerjaan = '-';
+                    }
+
+                    // -----batas suci-------
+                    $pattern = "/(?<=negaraan ).*/i";
+                    $kewarganegaraan = $new_pattern[13];
+                    $isExisted = preg_match($pattern, $kewarganegaraan, $matches);
+                    if ($isExisted == 1) {
+                        $kewarganegaraan = $matches[0];
+                        $pattern = "/[a-z]+/i";
+                        preg_match($pattern, $kewarganegaraan, $anjay);
+
+                        $kewarganegaraan = $anjay[0];
+                    } else {
+                        $kewarganegaraan = '-';
+                    }
+                    // dd($new_pattern);
+                    $ktp = [
+                        "provinsi" => $provinsi,
+                        "kota" => $kota,
+                        "nik" => $nik,
+                        "nama" => $nama,
+                        "tempat_lahir" => $tempat_lahir,
+                        "tanggal_lahir" => $tanggal_lahir,
+                        "kelamin" => $gender,
+                        "golongan_darah" => $golongan_darah,
+                        "alamat" => $alamat,
+                        "rt" => $rt,
+                        "rw" => $rw,
+                        "kelurahan" => $kelurahan,
+                        "kecamatan" => $kecamatan,
+                        "agama" => $agama,
+                        "perkawinan" => $perkawinan,
+                        "pekerjaan" => $pekerjaan,
+                        "kewarganegaraan" => $kewarganegaraan
+                    ];
+
+                    return response()->json([
+                        'status' => true,
+                        'message' => 'Upload file berhasil',
+                        'data' => $ktp
+                    ]);
+                }
             }
-        } else {
-            return response()->json([
-                'status' => false,
-                'message' => 'failed',
-                'data' => null
-            ]);
         }
     }
 

ade546dbcf1476162ea9d1acbbe923782dc2ffa3
endjiansyah <endji1507@gmail.com>
2023-01-05 10:05:55 +0700

    buat migration

---
+++ database/migrations/2023_01_05_025231_create_identity_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdentityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identity', function (Blueprint $table) {
+            $table->id();
+            $table->string("nik", 16);
+            $table->string("nama", 255);
+            $table->string("tempat_lahir", 100);
+            $table->date("tanggal_lahir");
+            $table->string("jenis_kelamin", 1);
+            $table->text("alamat");
+            $table->string("rt", 3);
+            $table->string("rw", 3);
+            $table->string("kelurahan", 50);
+            $table->string("kecamatan", 50);
+            $table->string("kota", 50);
+            $table->string("provinsi", 50);
+            $table->string("agama", 20);
+            $table->string("status_perkawinan", 20);
+            $table->string("pekerjaan", 100);
+            $table->string("kewarganegaraan", 50);
+            $table->string("golongan_darah", 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('identity');
+    }
+}

9d9d8d4b2730b63aff2734bbc4dd9da47f660698
khoiruddoa <khoiruddoa1995@gmail.com>
2023-01-18 15:56:44 +0700

    fix error jenis kelamin

--- app/Http/Controllers/IdcardController.php
+++ app/Http/Controllers/IdcardController.php
@@ -345,10 +345,24 @@ class IdcardController extends Controller
                             $gender = 1;
                         }
                     } else {
-                        $gender = '';
+                        $gender =  substr($nik, 6, 2);
+                        $gender = (int)$gender;
+
+                        if ($gender > 32) {
+                            $gender = 0;
+                        } else {
+                            $gender = 1;
+                        }
                     }
                 } else {
-                    $gender = '';
+                    $gender =  substr($nik, 6, 2);
+                    $gender = (int)$gender;
+
+                    if ($gender > 32) {
+                        $gender = 0;
+                    } else {
+                        $gender = 1;
+                    }
                 }
 
                 // -----batas suci-------

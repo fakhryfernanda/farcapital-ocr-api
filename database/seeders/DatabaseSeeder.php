98c56d58646dff008b3679c5a6f316794e90dfd5
endjiansyah <endji1507@gmail.com>
2023-01-27 07:45:07 +0700

    hapus seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,35 +34,5 @@ class DatabaseSeeder extends Seeder
             "id_role" => "1",
             "valid" => "1"
         ]);
-
-        User::query()->create([
-            "email" => "endji@gmail.com",
-            "password" => "endji",
-            "id_role" => "2",
-            "valid" => "1"
-        ]);
-
-        Identity::query()->create([
-            "id_user" => "2",
-            "nik" => "1234567812345678",
-            "nama" => "Endjiansyah",
-            "tempat_lahir" => "Jepara",
-            "tanggal_lahir" => "2020-10-10",
-            "jenis_kelamin" => "1",
-            "alamat" => "Jalan gunung batu no 3",
-            "rt" => "004",
-            "rw" => "002",
-            "kelurahan" => "Tegal Alur",
-            "kecamatan" => "Gunung Batu",
-            "kota" => "Jepara",
-            "provinsi" => "Jawa Tengah",
-            "agama" => "Islam",
-            "status_perkawinan" => "Belum Kawin",
-            "pekerjaan" => "Pelajar/Mahasiswa",
-            "kewarganegaraan" => "WNI",
-            "golongan_darah" => "B",
-            "ktp" => "KTP.jpg",
-            "foto" => "foto.jpg"
-        ]);
     }
 }
